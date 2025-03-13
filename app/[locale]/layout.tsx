fb76d7e06b14f8ccb3867fe9bdd0bce02f1ffe4f
Erickson-Da-Veiga <ericksondaveiga10@gmail.com>
2025-03-13 20:57:28 +0000

    updated title

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -11,6 +11,11 @@ export default function LocaleLayout({
 }) {
   return (
     <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Erickson Da Veiga | Personal Website</title>
+      </head>
       {/* Altere dinamicamente caso necessário */}
       <body className="">
         <Header />
--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// module.exports = {
+//   assetPrefix: './',
+//   basePath: 'portifolio-3.0',
+//   trailingShash: true,
+// };
+
+// module.exports = nextConfig;
+
 export default nextConfig;
